1bc7bd42bd3e672ba2d7b299fa2a71285aa01d07
SVSalenko <S.V.Salenko@gmail.com>
2019-09-16 08:47:52 +0300

    Added use of logger

--- app/code/Shellpea/Event/Observer/Event.php
+++ app/code/Shellpea/Event/Observer/Event.php
@@ -5,11 +5,20 @@ namespace Shellpea\Event\Observer;
 class Event implements \Magento\Framework\Event\ObserverInterface
 {
 
+  protected $logger;
+
+  public function __construct(\Psr\Log\LoggerInterface $logger)
+  {
+      $this->logger = $logger;
+  }
+
   public function execute(\Magento\Framework\Event\Observer $observer)
   {
     $request = $observer->getEvent()->getRequest();
     $url = $request->getPathInfo();
-    file_put_contents(BP.'/var/log/test.log', $url."\n",FILE_APPEND);
+    $url = "URL: " . $url;
+    //file_put_contents(BP.'/var/log/test.log', $url."\n",FILE_APPEND);
+    $this->logger->info($url);
   }
 
 }
